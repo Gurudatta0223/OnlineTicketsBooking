c2570d4637e1a1332956a63f17c21899c0d1d42f
Gurudatta <125348533+Gurudatta0223@users.noreply.github.com>
2023-03-05 12:00:30 +0530

    Update OnlineTicketCounter.java

--- OnlineTicketCounter.java
+++ OnlineTicketCounter.java
@@ -5,5 +5,6 @@ abstract class OnlineTicketCounter {
 	 abstract  void bookTickets(int noTickets);
 	 abstract void cancelTickets(int n);
 	 abstract  void checkTickets();
-	  int bookedTickets;
+	 
+	
 }
